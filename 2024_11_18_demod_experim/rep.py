25a2b51570d8129f9c2618f16e0580942c952088
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-11-21 16:03:15 -0500

    messy commit including colors

--- 2024_11_18_demod_experim/rep.py
+++ 2024_11_18_demod_experim/rep.py
@@ -1,6 +1,7 @@
 import zmq
 from shared import getTurtle
 import json
+from threading import Thread
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -16,33 +17,55 @@ def zmqsock(port):
     return socket
 
    
-def serialize(players: dict) -> str:
+def serialize(players: "dict[str, Turtle]") -> str:
     def do():
         for id_, turt in players.items():
             x, y = turt.pos()
             yield [id_, x, y]
+    def docol():
+        for id_, turt in players.items():
+            pc = turt.pencolor()
+            fc = turt.fillcolor()
+            yield [id_, pc, fc]
     locations = list(do())
-    return json.dumps({"locations": locations})
+    colors = list(docol())
+    return json.dumps({"locations": locations, "colors": colors})
 
 
 def updateAll(sock, players):
     message: str = sock.recv().decode()
-    cmd, turtid, clientdata = message.split(",", 2)
-    if cmd == "MOVE":
-        xs, ys = clientdata.split(",")
-        turt = getTurtle(turtid, players)
-        turt.goto(int(xs), int(ys))
-    else:
-        print("cmd", cmd, "not implemented")
+    try:
+        turtid, cmd, clientdata = message.split(" ", 2)
+        if cmd == "MOVE":
+            turt = getTurtle(turtid, players)
+            xs, ys = clientdata.split(" ")
+            turt.goto(int(xs), int(ys))
+        elif cmd == "SETPENCOLOR":
+            turt = getTurtle(turtid, players)
+            turt.pencolor(clientdata)
+        elif cmd == "SETFILLCOLOR":
+            turt = getTurtle(turtid, players)
+            turt.fillcolor(clientdata)
+        else:
+            print("cmd", cmd, "not implemented")
+    except ValueError:
+        print("Invalid command")
     # elif cmd == "STAMP":
     #     turt = getTurtle(f"{turtid}_stamp_{random.randint(0, 100000)}", players)
     sock.send_string(serialize(players))
+
+# def cmdloop():
+#     while True:
+#         cmd = input("Admin command: ")
+#         if cmd.startswith("kick"):
+            
 
 
 def main():
     print("Waiting for connections. You may want to use `ip a` to check your own ip address")
     players: "dict[str, Turtle]" = {}
     sock = zmqsock(5555)
+    # Thread(target=cmdloop).start()
     while True:
         updateAll(sock, players)
 
--- 2024_11_18_demod_experim/student.py
+++ 2024_11_18_demod_experim/student.py
@@ -4,13 +4,13 @@ import random
 from threading import Thread
 import time
 import json
+from queue import SimpleQueue
 
 from shared import getTurtle
 
 
 # Often, ADDR is set to an ip address, for example, "192.168.1.2"
 ADDR = "localhost"
-
 
 def getsock():
     context = zmq.Context()
@@ -24,44 +24,69 @@ def unserialize(sockdata: str) -> "list[tuple[str, int, int]]":
     return sockjson["locations"]
 
 
-def updatepositions(sock, players, myid):
-    xf, yf = turtle.pos()
+def updatepositions():
+    xf, yf = myturtle.pos()
     x = int(xf)
     y = int(yf)
-    sock.send(f'MOVE,{myid},{x},{y}'.encode())
-    locations = unserialize(sock.recv())
-    # Update the local view of where everyone else is.
-    for turtid, x, y in locations:
-        if turtid != myid:
-            t = getTurtle(turtid, players)
-            t.goto(x, y)
+    cmd = f'{myid} MOVE {x} {y}'
+    cmdqueue.put(cmd)
+
+
+def readqueue():
+    def sendcmd_and_update(cmd: str):
+        sock.send(cmd.encode())
+        locations = unserialize(sock.recv())
+        # Update the local view of where everyone else is.
+        for turtid, x, y in locations:
+            if turtid != myid:
+                t = getTurtle(turtid, players)
+                t.goto(x, y)
+
+    while True:
+        cmd = cmdqueue.get(block=True)
+        sendcmd_and_update(cmd)
 
 
 def updateforever():
-    sock = getsock()
-    players = {}
-    myid = str(random.randint(0, 1000000))
-
     while True:
-        updatepositions(sock, players, myid)
+        updatepositions()
         time.sleep(0.1)
-        
+
+def cmds():
+    while True:
+        cmd = input("Command or h for help: ")
+        if cmd == "h":
+            print(
+                "Examples:\n"
+                "SETPENCOLOR red\n"
+                "SETFILLCOLOR green\n")
+        else:
+            cmdwithid = f"{myid} {cmd.upper()}"
+            cmdqueue.put(cmdwithid)
+
 
 def left():
-    turtle.left(90)
+    myturtle.left(90)
 
 def right():
-    turtle.right(90)
+    myturtle.right(90)
 
 def fwd():
-    turtle.forward(5)
+    myturtle.forward(5)
 
 
-turtle.speed(0)
-turtle.penup()
+sock = getsock()
+players = {}
+myid = str(random.randint(0, 1000000))
+cmdqueue = SimpleQueue()
+myturtle = getTurtle(myid, players)
+myturtle.speed(0)
+myturtle.penup()
 turtle.onkeyrelease(left, "a")
 turtle.onkeyrelease(fwd, "w")
 turtle.onkeyrelease(right, "d")
 turtle.listen()
 Thread(target=updateforever).start()
+Thread(target=readqueue).start()
+Thread(target=cmds).start()
 turtle.mainloop()
