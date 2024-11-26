dcc7f695a42dbe0bf46c6958b711b489631ee942
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-11-26 16:18:01 -0500

    visualizing mod demod: basic functionality complete

--- 2024_11_26_visualizing_blocks/visualizing_mod_demod.py
+++ 2024_11_26_visualizing_blocks/visualizing_mod_demod.py
@@ -1,8 +1,13 @@
+from __future__ import annotations
 from functools import lru_cache
 from math import sin
-from typing import Callable
-from more_itertools import flatten, grouper
-from guizero import App, Drawing
+from typing import Callable, TypeVar, Generic, Any
+from more_itertools import flatten, grouper  # type: ignore[import-untyped]
+from guizero import App, Drawing  # type: ignore[import-untyped]
+
+
+T = TypeVar("T")
+U = TypeVar("U")
 
 
 def lmap(f, x):
@@ -11,11 +16,15 @@ def lmap(f, x):
 def lflatten(x):
     return list(flatten(x))
 
+def lfilter(f: Callable[[T], bool], x: "list[T]") -> "list[T]":
+    return list(filter(f, x))
+
 def lgrouper(n, iterable):
     return list(grouper(n, iterable))
 
-def bin_(x):
-    return f"{x:08b}"
+def lzip(a, b):
+    return list(zip(a, b))
+
 
 def int2(x: "list[str]") -> int:
     """Given a list of bits, treat the list as a single
@@ -23,7 +32,7 @@ def int2(x: "list[str]") -> int:
     joi = "".join(x)
     return int(joi, 2)
 
-def longshort(x) -> "list[int]":
+def longshort(x: str) -> "list[int]":
     if x == "0":
         return [1, 0]
     elif x == "1":
@@ -54,16 +63,16 @@ def morse_mod(bits: "list[str]"):
     return b
 
 
-def modulate(plain_text_msg: str) -> "list[str]":
-    a = list(plain_text_msg)
-    b = lmap(ord, a)
-    # print("Converted to decimal:", a)
-    c = lmap(bin_, b)
-    # print("Converted to binary:", b)
-    d = lflatten(c)
-    # print("Flattened:", d)
-    e = morse_mod(d)
-    return e
+# def modulate(plain_text_msg: str) -> "list[str]":
+#     a = list(plain_text_msg)
+#     b = lmap(ord, a)
+#     # print("Converted to decimal:", a)
+#     c = lmap(bin_, b)
+#     # print("Converted to binary:", b)
+#     d = lflatten(c)
+#     # print("Flattened:", d)
+#     e = morse_mod(d)
+#     return e
 
 
 def demodulate(mod_msg: "list[str]") -> str:
@@ -73,67 +82,157 @@ def demodulate(mod_msg: "list[str]") -> str:
     d = "".join(c)
     return d
 
-class Datum:
-    def __init__(self, v):
+
+class Datum(Generic[T]):
+    def __init__(self, v: T, fullwidth: int):
         self.v = v
-        self.x = 0
+        self.x = fullwidth + 100
+        self.y = 10
+        self.done_block_ids: "list[int]" = []
+        self.fullwidth = fullwidth
 
-    def update(self, cblocks: "list[CBlock]"):
-        self.x += 2
-        if self.x > cblocks[0].x:
-            self.v = cblocks[0].func(self.v)
-            import sys
-            sys.exit()
+    def newdat(self, newval, blid) -> "Datum":
+        """Return a new Datum with the current
+        attrs copied except for those provided in the args."""
+        newdatum = Datum(newval, self.fullwidth)
+        newdatum.x = self.x
+        newdatum.done_block_ids = self.done_block_ids + [blid]
+        return newdatum
 
-    def draw(self):
-        drawing.text(self.x, 0, self.v)
+
+    def apply_current_block(self, sblocks: "list[SBlock]") -> "list[Datum]":
+        """Given current position, determine what processing block
+        needs to happen now (if any)."""
+        def seen_and_not_surpassed(cb: SBlock[Any, Any]) -> bool:
+            return (
+                id(cb) not in self.done_block_ids and
+                self.x < cb.x and
+                self.y == cb.y
+            )
+
+        seennotsurp = lfilter(seen_and_not_surpassed, sblocks)
+        if seennotsurp == []:
+            return [self]  # no blocks seen; nothing to do
+        elif len(seennotsurp) > 1:
+            raise NotImplementedError("Blocks are too close together because two were seen simultaneously.")
+        else:
+            currentblock = seennotsurp[0]
+            f = currentblock.func
+            newval = f(self.v)
+            return [self.newdat(newval, id(currentblock))]
+
+    def updatepos(self):
+        self.x -= 2
+        if self.x < -70:
+            self.x = self.fullwidth
+            self.y += 100
+
+    def update(self, sblocks: "list[SBlock]") -> "list[Datum]":
+        self.updatepos()
+        newdat = self.apply_current_block(sblocks)
+        return newdat
+
+    def draw(self, drawing: Drawing) -> None:
+        if isinstance(self.v, str):
+            drawing.text(self.x, self.y, self.v)
+        elif isinstance(self.v, (int, float)):
+            drawing.rectangle(self.x, self.y, self.x+5, self.y+5)
+        else:
+            drawing.text(self.x, self.y, repr(self.v))
         
 
-class CBlock:
-    def __init__(self, func: Callable, x: int, y: int):
+class SBlock(Generic[T, U]):
+    def __init__(self, func: Callable[[T], U], pos: "tuple[int, int]"):
         self.func = func
-        self.x = x
-        self.y = y
-    def draw(self):
-        drawing.rectangle(self.x, self.y, self.x+100, self.y+30, color="cyan", outline=True, outline_color="black")
-        drawing.text(self.x, self.y, self.func.__name__)
+        self.x = pos[0]
+        self.y = pos[1]
 
-class DrawingTracker:
-    def __init__(self, msg: "list[str]", cblocks: "list[CBlock]"):
-        self.cblocks = cblocks
-        self.waiting: "list[Datum]" = lmap(Datum, msg)
-        self.displaying: "list[Datum]" = []
+    def draw(self, drawing: Drawing):
+        drawing.rectangle(self.x-4, self.y-4, self.x+100, self.y+30, color="cyan", outline=True, outline_color="black")
+        drawing.text(self.x, self.y, self.func.__name__)
+    
+    def __repr__(self):
+        return f"<SBlock id: {id(self)} funcname: {self.func.__name__} x: {self.x}  y: {self.y}>"
+
+
+class DrawingTracker(Generic[T]):
+    def __init__(self, data: "list[T]", sblocks: "list[SBlock]", fullwidth: int, drawing: Drawing, dataspacing: int):
+        makeDatum = lambda v: Datum(v, fullwidth)
+        self.sblocks = sblocks
+        self.waiting: "list[Datum[T]]" = lmap(makeDatum, data)
+        self.displaying: "list[Datum[Any]]" = []
+        self.dataspacing = dataspacing
         self.spacing = 0
+        self.drawing = drawing
 
     def add_to_displayed(self):
+        """Modifies self.waiting, self.displaying, and self.spacing."""
         if not self.waiting:
             return
-        if self.spacing > 50:
+        if self.spacing > self.dataspacing:
             d = self.waiting.pop(0)
             self.displaying.append(d)
             self.spacing = 0 
         self.spacing += 2
 
     def updatetext(self):
-        drawing.clear()
+        self.drawing.clear()
         self.add_to_displayed()
+        self.displaying = lflatten([d.update(self.sblocks) for d in self.displaying])
         for d in self.displaying:
-            d.update(self.cblocks)
-            d.draw()
-        for cb in self.cblocks:
-            cb.draw()
-        
-# plain_text_msg = input("Type a message: ")
-def times_two(x):
-    return x * 2
-app = App()
-drawing = Drawing(app, width="fill", height="fill")
-plain_text_msg = "abc"
-cblocks = [CBlock(times_two, 50, 0)]
-dtr = DrawingTracker(list(plain_text_msg), cblocks)
-drawing.repeat(20, dtr.updatetext)
-app.display()
-modulated = modulate(plain_text_msg)
+            d.draw(self.drawing)
+        for sb in self.sblocks:
+            sb.draw(self.drawing)
+
+
+class Slowgraph:
+    def __init__(self, initial_data: "list[T]", funcs: "list[Callable]", dataspacing: int = 150):
+        self.app = App(width=800)
+        drawing = Drawing(self.app, width="fill", height="fill")
+        sblocks = Slowgraph.place_blocks(funcs)
+        self.dtr = DrawingTracker(initial_data, sblocks, self.app.width, drawing, dataspacing)
+        drawing.repeat(20, self.dtr.updatetext)
+
+    @staticmethod
+    def place_blocks(funcs: "list[Callable]"):
+        grid = [
+            (600, 10),  (350, 10),  (100, 10),
+            (600, 110), (350, 110), (100, 110),
+        ]
+        makesblock = lambda x: SBlock(*x)
+        funcgrid = lzip(funcs, grid)
+        sblocks = lmap(makesblock, funcgrid)
+        return sblocks
+
+    def display(self):
+        self.app.display()
+
+
+
+def main():
+
+    def ord_(x: str) -> str:
+        return str(ord(x))
+
+    def bin_(x: str) -> str:
+        intx = int(x)
+        return f"{intx:08b}"
+
+    funcs = [ord_, bin_, list]
+    msg = "abc"
+    listymsg = list(msg)
+    slowg = Slowgraph(listymsg, funcs, dataspacing=300)
+    slowg.display()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# modulated = modulate(plain_text_msg)
 # print(f"Modulated message: {modulated}")
 # demod = demodulate(modulated)
 # print(f"Demodulated message: {demod}")
