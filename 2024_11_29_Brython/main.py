11201c7e2d606655dd49d0428fd5da97ff4e073c
wolverine-is-the-best <127511752+wolverine-is-the-best@users.noreply.github.com>
2024-12-02 15:20:13 -0500

    added 2 more processes and some aesthetic changes removed unused imports

--- 2024_11_29_Brython/main.py
+++ 2024_11_29_Brython/main.py
@@ -1,8 +1,9 @@
 from browser import document, alert
-from browser.html import TEXTAREA, P, TABLE, TR, TD, DIV, STYLE, BUTTON, PRE
+from browser.html import P, TABLE, TR, TD, DIV, STYLE, PRE
 
 
 color_red = "#EB212E"
+color_grey = "#D3D3D3"
 
 def displayError(f):
     def do(*args, **kwargs):
@@ -21,12 +22,12 @@ def updatelocal(proc, inp, out, varname, funcs):
     try:
         gbls = {varname: eval(inp.text)}
     except Exception as e:
-        tx_sty(f"Unable to evaluate input from previous cell: '{inp.text}'", color_red)
+        tx_sty(f"Unable to evaluate input: '{inp.text}'", color_red)
         return
     
     code: str = proc.text
     if code.strip() == "":
-        tx_sty("______", "#eee")
+        tx_sty("_", color_grey)
         return
     
     try:
@@ -40,27 +41,33 @@ def main():
     def updateall(ev):
         updatelocal(primproc, priminp, primout, "a", funcs)
         updatelocal(proc2, primout, out2, "b", funcs)
-        finalcode.innerHTML = "a = "+ priminp.text + "<br>b = " + primproc.text.strip() + "<br>c = " + proc2.text.strip()
+        updatelocal(proc3, out2, out3, "c", funcs)
+        updatelocal(proc4, out3, out4, "d", funcs)
+        finalcode.innerHTML = "a = "+ priminp.text + "<br>b = " + primproc.text.strip() + "<br>c = " + proc2.text.strip()+ "<br>d = " + proc3.text.strip()+ "<br>e = " + proc4.text.strip()
 
     document <= STYLE("* { font-family: monospace; font-size: 20px; }")
 
     document <= P("Functions (doesn't work yet):")
     funcs = DIV(PRE("""
 def lmap(f,x):
-    return list(map(f,x))"""), style="vertical-align: middle; border: 1px solid black;", contenteditable=True)
+    return list(map(f,x))"""), style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
     document <= funcs
     document <= P("Process demo:")
-    priminp = DIV(style="vertical-align: middle; border: 1px solid black;", contenteditable=True)
-    primproc = DIV(style="vertical-align: middle; border: 1px solid orange;", contenteditable=True)
-    primout = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid orange;")
-    proc2 = DIV(style="vertical-align: middle; border: 1px solid orange;", contenteditable=True)
-    out2 = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid orange;")
+    priminp = DIV(style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
+    primproc = DIV(style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
+    primout = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid blue;")
+    proc2 = DIV(style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
+    out2 = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid blue;")
+    proc3 = DIV(style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
+    out3 = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid blue;")
+    proc4 = DIV(style="vertical-align: middle; border: 1px solid blue;", contenteditable=True)
+    out4 = DIV("&nbsp;", style="vertical-align: middle; border: 1px solid blue;")
     
     document <= TABLE([
         TR([
             TD(),
-            TD("Process",style='text-align: center; min-width: 100px;'),
-            TD("I/O",style='text-align: center; min-width: 100px;')
+            TD("Process",style='text-align: center; min-width: 200px;'),
+            TD("I/O",style='text-align: center; min-width: 200px;')
         ]),
         TR([
             TD("a =&nbsp;",style='text-align: right'),
@@ -75,16 +82,28 @@ def lmap(f,x):
         TR([
             TD("c =&nbsp;"),
             TD(proc2),
-            TD(out2)
+            TD(out2),
+        ]),
+        TR([
+            TD("d =&nbsp;"),
+            TD(proc3),
+            TD(out3),
+        ]),
+        TR([
+            TD("e =&nbsp;"),
+            TD(proc4),
+            TD(out4)
         ])
     ])
 
-    finalcode = DIV(contenteditable=True, style="vertical-align: middle; border: 1px solid orange; margin-top: 1em;")
+    finalcode = DIV(contenteditable=True, style="vertical-align: middle; border: 1px solid blue; margin-top: 1em;")
     document <= finalcode
 
     priminp.onkeyup = displayError(updateall)
     primproc.onkeyup = displayError(updateall)
     proc2.onkeyup = displayError(updateall)
+    proc3.onkeyup = displayError(updateall)
+    proc4.onkeyup = displayError(updateall)
 
 
 main()
