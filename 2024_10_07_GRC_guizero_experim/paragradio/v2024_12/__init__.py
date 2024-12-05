1134cfb950ae3a5ba29ebacc6e49ffe877084ce7
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-12-05 10:36:36 -0500

    finish adding simspecan

--- 2024_10_07_GRC_guizero_experim/paragradio/v2024_12/__init__.py
+++ 2024_10_07_GRC_guizero_experim/paragradio/v2024_12/__init__.py
@@ -2,7 +2,13 @@ from paragradio.v2024_12._internal import (
     PGR_basicsourcesinkwater,
     P_turtle_xsquared,
     PGR_specan,
+    PGR_simspecan,
 )
 
 # this is for mypy
-__all__ = ["PGR_basicsourcesinkwater", "P_turtle_xsquared", "PGR_specan"]
+__all__ = [
+    "PGR_basicsourcesinkwater",
+    "P_turtle_xsquared",
+    "PGR_specan",
+    "PGR_simspecan",
+]
