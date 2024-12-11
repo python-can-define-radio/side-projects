2b3d0ecb9bb79afb7f5f7e04643bed52e306e257
wolverine-is-the-best <127511752+wolverine-is-the-best@users.noreply.github.com>
2024-12-11 11:13:42 -0500

    added wbfm_rx for v3_10 and created the __init__ for that folder

---
+++ 2024_10_07_GRC_guizero_experim/paragradio/v2024_12/_internal/wbfm/__init__.py
@@ -0,0 +1,10 @@
+from gnuradio import gr
+
+v = gr.version()
+assert isinstance(v, str)
+if v.startswith("3.8"):
+    from .wbfm_rx_gr3_8 import wbfm_rx_gr3_8 as wbfm_rx
+elif v.startswith("3.10"):
+    from .wbfm_rx_gr3_10 import wbfm_rx_gr3_10 as wbfm_rx
+else:
+    raise NotImplementedError(f"gnuradio version {v} is not known to be supported; you can submit a pull request on URL HERE FOR PARAGRADIO ON GITHUB")
--- 2024_10_07_GRC_guizero_experim/paragradio/v2024_12/_internal/wbfm/wbfm_rx_gr3_10.py
+++ 2024_10_07_GRC_guizero_experim/paragradio/v2024_12/_internal/wbfm/wbfm_rx_gr3_10.py
@@ -5,50 +5,41 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
-# Author: taurean.j.byrd31
-# GNU Radio version: 3.8.1.0
+# Title: wbfm_rx_gr3_10
+# GNU Radio version: 3.10.7.0
 
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
+from packaging.version import Version as StrictVersion
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import audio
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
+from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import osmosdr
 import time
-from gnuradio import qtgui
+import sip
 
-class wbfm_rx(gr.top_block, Qt.QWidget):
+
+
+class wbfm_rx_gr3_10(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet")
+        gr.top_block.__init__(self, "wbfm_rx_gr3_10", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
+        self.setWindowTitle("wbfm_rx_gr3_10")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -61,15 +52,15 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "wbfm_rx")
+        self.settings = Qt.QSettings("GNU Radio", "wbfm_rx_gr3_10")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
                 self.restoreGeometry(self.settings.value("geometry").toByteArray())
             else:
                 self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -82,25 +73,27 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        _hardware_filter_check_box = Qt.QCheckBox('hardware_filter')
+
+        _hardware_filter_check_box = Qt.QCheckBox("'hardware_filter'")
         self._hardware_filter_choices = {True: True, False: False}
         self._hardware_filter_choices_inv = dict((v,k) for k,v in self._hardware_filter_choices.items())
         self._hardware_filter_callback = lambda i: Qt.QMetaObject.invokeMethod(_hardware_filter_check_box, "setChecked", Qt.Q_ARG("bool", self._hardware_filter_choices_inv[i]))
         self._hardware_filter_callback(self.hardware_filter)
         _hardware_filter_check_box.stateChanged.connect(lambda i: self.set_hardware_filter(self._hardware_filter_choices[bool(i)]))
-        self.top_grid_layout.addWidget(_hardware_filter_check_box)
+        self.top_layout.addWidget(_hardware_filter_check_box)
         self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
                 interpolation=int(48e3),
                 decimation=int(samp_rate),
-                taps=None,
-                fractional_bw=None)
+                taps=[],
+                fractional_bw=0)
         self.qtgui_waterfall_sink_x_1 = qtgui.waterfall_sink_c(
             1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            window.WIN_BLACKMAN_hARRIS, #wintype
             center_freq, #fc
             samp_rate, #bw
             "", #name
-            1 #number of inputs
+            1, #number of inputs
+            None # parent
         )
         self.qtgui_waterfall_sink_x_1.set_update_time(0.10)
         self.qtgui_waterfall_sink_x_1.enable_grid(False)
@@ -125,15 +118,17 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
 
         self.qtgui_waterfall_sink_x_1.set_intensity_range(-140, 10)
 
-        self._qtgui_waterfall_sink_x_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_1.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_1_win)
+        self._qtgui_waterfall_sink_x_1_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_1.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_1_win)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
             1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
             samp_rate, #bw
             "Original Spectrum", #name
-            1 #number of inputs
+            1, #number of inputs
+            None # parent
         )
         self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
         self.qtgui_waterfall_sink_x_0.enable_grid(False)
@@ -158,13 +153,15 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
 
         self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
 
-        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            1 #number of inputs
+            1, #number of inputs
+            None # parent
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
@@ -208,8 +205,8 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.osmosdr_source_0 = osmosdr.source(
             args="numchan=" + str(1) + " " + "hackrf=0"
         )
@@ -217,27 +214,29 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_sample_rate(samp_rate)
         self.osmosdr_source_0.set_center_freq(center_freq, 0)
         self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
         self.osmosdr_source_0.set_gain(0, 0)
         self.osmosdr_source_0.set_if_gain(if_gain, 0)
         self.osmosdr_source_0.set_bb_gain(50, 0)
         self.osmosdr_source_0.set_antenna('', 0)
-        self.osmosdr_source_0.set_bandwidth(hardware_filter * 2.75e6, 0)
+        self.osmosdr_source_0.set_bandwidth((hardware_filter * 2.75e6), 0)
         self.band_pass_filter_0 = filter.fir_filter_ccf(
             1,
             firdes.band_pass(
                 1,
                 samp_rate,
-                -100e3,
+                (-100e3),
                 100e3,
                 90e3,
-                firdes.WIN_HAMMING,
+                window.WIN_HAMMING,
                 6.76))
         self.audio_sink_0 = audio.sink(48000, '', True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
         	quad_rate=samp_rate,
         	audio_decimation=1,
         )
-
 
 
         ##################################################
@@ -251,9 +250,13 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
         self.connect((self.osmosdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
 
+
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "wbfm_rx")
+        self.settings = Qt.QSettings("GNU Radio", "wbfm_rx_gr3_10")
         self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
         event.accept()
 
     def get_samp_rate(self):
@@ -261,7 +264,7 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, -100e3, 100e3, 90e3, firdes.WIN_HAMMING, 6.76))
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, (-100e3), 100e3, 90e3, window.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
@@ -280,7 +283,7 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
     def set_hardware_filter(self, hardware_filter):
         self.hardware_filter = hardware_filter
         self._hardware_filter_callback(self.hardware_filter)
-        self.osmosdr_source_0.set_bandwidth(self.hardware_filter * 2.75e6, 0)
+        self.osmosdr_source_0.set_bandwidth((self.hardware_filter * 2.75e6), 0)
 
     def get_center_freq(self):
         return self.center_freq
@@ -292,7 +295,8 @@ class wbfm_rx(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=wbfm_rx, options=None):
+
+def main(top_block_cls=wbfm_rx_gr3_10, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
@@ -300,10 +304,15 @@ def main(top_block_cls=wbfm_rx, options=None):
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
+
     tb.start()
+
     tb.show()
 
     def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -313,12 +322,7 @@ def main(top_block_cls=wbfm_rx, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
-
 
 if __name__ == '__main__':
     main()
