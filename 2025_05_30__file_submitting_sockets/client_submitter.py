ed88fc8ae2c232c985e3e59bfd4330fe7b727537
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2025-05-30 11:51:11 -0400

    Update client_submitter.py: add func to send and disconnect

--- 2025_05_30__file_submitting_sockets/client_submitter.py
+++ 2025_05_30__file_submitting_sockets/client_submitter.py
@@ -1,1 +1,17 @@
+import socket
 
+
+def connect_tx_rx_close(tosend: str) -> str:
+    HOST = '10.50.129.202'
+    PORT = 50007
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.send(tosend.encode("utf8"))
+    recvd = s.recv().decode("utf8")
+    s.close()
+    return recvd
+
+
+sendthis = "?"
+serversaid = connect_tx_rx_close(sendthis)
+print("Server's reply:", serversaid)
