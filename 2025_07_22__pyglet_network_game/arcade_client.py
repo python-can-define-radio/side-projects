95cba75c9ca82b35a02b6b1a1a632e4f0878632f
wolverine-is-the-best <127511752+wolverine-is-the-best@users.noreply.github.com>
2025-08-26 10:48:45 -0400

    combined user_selection into the client (working copy)

--- 2025_07_22__pyglet_network_game/arcade_client.py
+++ 2025_07_22__pyglet_network_game/arcade_client.py
@@ -1,6 +1,7 @@
 import arcade
 import zmq
-from shared_constants import SCREEN_WIDTH, SCREEN_HEIGHT, TILE_SCALING, PLAYER_SCALING
+from shared_constants import SCREEN_WIDTH, SCREEN_HEIGHT, TILE_SCALING, PLAYER_SCALING, AVATAR_IMAGES
+
 
 
 context = zmq.Context()
@@ -14,93 +15,194 @@ from_srv_sock.connect("tcp://localhost:5556")
 from_srv_sock.setsockopt_string(zmq.SUBSCRIBE, "")
 
 
-class MyGame(arcade.Window):
-    """Main application class."""
+class MyGameView(arcade.View):
+    def __init__(self, username: str, avatar_index: int):
+        super().__init__()
+        self.username = username
+        self.avatar_index = avatar_index
 
-    def __init__(self):
-        """
-        Initializer
-        """
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Game client", resizable=True)
+        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+
         self.setup()
 
     def setup(self):
-        """Set up the game and initialize the variables."""
-
-        # Map name
         map_name = ":resources:tiled_maps/level_1.json"
-
-        # Load in TileMap
         self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING)
-
-        # Initiate New Scene with our TileMap, this will automatically add all layers
-        # from the map as SpriteLists in the scene in the proper order.
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
-        # Set up the player
-        self.player_sprite = arcade.Sprite(
-            ":resources:images/animated_characters/female_person/femalePerson_idle.png",
-            PLAYER_SCALING,
-        )
-
-        # Starting position of the player
+        # Set up the player with selected avatar
+        avatar_image = AVATAR_IMAGES[self.avatar_index]
+        self.player_sprite = arcade.Sprite(avatar_image, PLAYER_SCALING)
         self.player_sprite.center_x = 196
         self.player_sprite.center_y = 128
         self.scene.add_sprite("Player", self.player_sprite)
 
-        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        # Background color
+        arcade.set_background_color((200, 200, 255))
 
-        # --- Other stuff
-        # Set the background color
-        self.background_color = (200, 200, 255)
-
-    def on_resize(self, width, height):
-        """Resize window"""
-        self.camera.resize(width, height)
+    def on_show_view(self):
+        # Reset camera when view is shown
+        self.camera.resize(self.window.width, self.window.height)
 
     def on_draw(self):
-        """Render the screen."""
         self.clear()
         self.camera.use()
         self.scene.draw()
+
+        # Draw username on screen (optional)
+        arcade.draw_text(f"Username: {self.username}", 10, self.window.height - 30,
+                         arcade.color.BLACK, 14)
+
+    def on_resize(self, width, height):
+        self.camera.resize(width, height)
 
     def on_key_press(self, key, modifiers):
         to_srv_sock.send_json({"keypress": key})
 
     def on_key_release(self, key, modifiers):
         to_srv_sock.send_json({"keyrelease": key})
-        
+
     def pan_camera_to_user(self, panning_fraction: float = 1.0):
-        """
-        Manage Scrolling
-
-        :param panning_fraction: Number from 0 to 1. Higher the number, faster we
-                                 pan the camera to the user.
-        """
-
-        # This spot would center on the user
         screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.player_sprite.center_y - (
-            self.camera.viewport_height / 2
-        )
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
         user_centered = screen_center_x, screen_center_y
         self.camera.move_to(user_centered, panning_fraction)
 
     def on_update(self, delta_time):
-        """Movement and game logic"""
-        try:
-            message: dict = from_srv_sock.recv_json(flags=zmq.NOBLOCK)
-            self.player_sprite.center_x = message["playerx"]
-            self.player_sprite.center_y = message["playery"]
-        except zmq.Again:
-            pass
-        # update_player_position_based_on_the_server_info()
+        latest_message = None
+        while True:
+            try:
+                message = from_srv_sock.recv_json(flags=zmq.NOBLOCK)
+                latest_message = message  # Always keep the latest one
+            except zmq.Again:
+                break  # No more messages
+
+        if latest_message:
+            self.player_sprite.center_x = latest_message["playerx"]
+            self.player_sprite.center_y = latest_message["playery"]
         self.pan_camera_to_user(panning_fraction=0.12)
 
 
+class UsernameSelectionView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+        self.button_text = "Submit"
+
+        self.button_x = SCREEN_WIDTH // 2
+        self.button_y = SCREEN_HEIGHT // 4  # Submit button lower on screen
+        self.button_width = 200
+        self.button_height = 50
+
+        self.username = ""
+        self.textbox_x = SCREEN_WIDTH // 2 + 40
+        self.textbox_y = SCREEN_HEIGHT - 225  # Textbox near top
+        self.textbox_width = 200
+        self.textbox_height = 30
+        self.textbox_active = False
+
+        self.avatar_sprites = []
+        self.avatar_width = 64
+        self.avatar_height = 64
+        self.avatar_y = self.button_y + 100  # Avatars above button
+        self.selected_avatar_index = None
+        self.cursor_default = self.window.get_system_mouse_cursor(self.window.CURSOR_DEFAULT)
+        self.cursor_text = self.window.get_system_mouse_cursor(self.window.CURSOR_TEXT)
+
+        for i, avatar_image in enumerate(AVATAR_IMAGES):
+            sprite = arcade.Sprite(avatar_image, scale=0.5)
+            sprite.center_x = (SCREEN_WIDTH // 2 - (len(AVATAR_IMAGES) * self.avatar_width // 2)
+                               + i * self.avatar_width + 50)
+            sprite.center_y = self.avatar_y
+            self.avatar_sprites.append(sprite)
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
+
+    def on_draw(self):
+        self.clear()
+
+        # Username label + textbox
+        arcade.draw_text("Username:", self.textbox_x - 210, self.textbox_y + 10, arcade.color.WHITE, 14)
+        arcade.draw_rectangle_filled(self.textbox_x, self.textbox_y + 15,
+                                     self.textbox_width, self.textbox_height, arcade.color.WHITE)
+        arcade.draw_text(self.username,
+                         self.textbox_x - self.textbox_width // 2 + 5,
+                         self.textbox_y + 10,
+                         arcade.color.BLACK, 14)
+
+        # Label: Select an Avatar
+        label_y = self.avatar_y + 60
+        arcade.draw_text("Select an Avatar", SCREEN_WIDTH // 2, label_y,
+                         arcade.color.WHITE, 16, anchor_x="center")
+
+        # Avatars with highlight
+        for i, sprite in enumerate(self.avatar_sprites):
+            if self.selected_avatar_index == i:
+                arcade.draw_rectangle_outline(sprite.center_x, sprite.center_y,
+                                              self.avatar_width + 10, self.avatar_height + 10,
+                                              arcade.color.YELLOW, 3)
+            sprite.draw()
+
+        # Submit button
+        arcade.draw_rectangle_filled(self.button_x, self.button_y,
+                                     self.button_width, self.button_height, arcade.color.BLUE)
+        arcade.draw_text(self.button_text,
+                         self.button_x, self.button_y,
+                         arcade.color.WHITE, 18,
+                         anchor_x="center", anchor_y="center")
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        inside = (
+            self.textbox_x - self.textbox_width // 2 <= x <= self.textbox_x + self.textbox_width // 2 and
+            self.textbox_y - self.textbox_height // 2 <= y <= self.textbox_y + self.textbox_height // 2
+        )
+        if inside:
+            self.window.set_mouse_cursor(self.cursor_text)
+        else:
+            self.window.set_mouse_cursor(self.cursor_default)
+
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        # Submit button click
+        if (self.button_x - self.button_width / 2 <= x <= self.button_x + self.button_width / 2 and
+                self.button_y - self.button_height / 2 <= y <= self.button_y + self.button_height / 2):
+            if self.selected_avatar_index is not None and self.username.strip():
+                # Pass username and avatar index to game view
+                game_view = MyGameView(
+                    username=self.username.strip(),
+                    avatar_index=self.selected_avatar_index
+                )
+                self.window.show_view(game_view)
+            else:
+                print("Please select an avatar and enter a username.")
+
+        # Check avatar clicks
+        for i, sprite in enumerate(self.avatar_sprites):
+            if sprite.collides_with_point((x, y)):
+                self.selected_avatar_index = i
+                print(f"Avatar selected: {AVATAR_IMAGES[i]}")
+
+        # Check textbox click
+        self.textbox_active = (
+            self.textbox_x - self.textbox_width // 2 <= x <= self.textbox_x + self.textbox_width // 2 and
+            self.textbox_y - self.textbox_height // 2 <= y <= self.textbox_y + self.textbox_height // 2
+        )
+
+    def on_key_press(self, symbol, modifiers):
+        if self.textbox_active:
+            if symbol == arcade.key.BACKSPACE:
+                self.username = self.username[:-1]
+            elif 32 <= symbol <= 126:
+                self.username += chr(symbol)
+
+
 def main():
-    """Get this game started."""
-    window = MyGame()
+    """Start the arcade window and show UsernameSelectionView"""
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Game Client", resizable=True)
+
+    selection_view = UsernameSelectionView()
+    window.show_view(selection_view)
     arcade.run()
 
 
--- 2025_07_22__pyglet_network_game/shared_constants.py
+++ 2025_07_22__pyglet_network_game/shared_constants.py
@@ -4,4 +4,8 @@ PLAYER_SCALING = 0.5
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-
+AVATAR_IMAGES = [
+    ":resources:images/animated_characters/female_person/femalePerson_idle.png",
+    ":resources:images/animated_characters/male_person/malePerson_idle.png",
+    ":resources:images/animated_characters/zombie/zombie_idle.png"
+]
