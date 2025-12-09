d8e97fb32874efea0eb1e8181ddbdef7d7d1d830
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2025-12-09 15:59:45 -0500

    delete more fdtd code and refactor

--- 2025_11_07__fastapi_web_game/pyodide_minimal.py
+++ 2025_11_07__fastapi_web_game/pyodide_minimal.py
@@ -5,57 +5,41 @@ from typing import TYPE_CHECKING
 from js import document, ImageData, Uint8ClampedArray  # type: ignore
 from pyodide.http import pyfetch  # type: ignore
 
-
-def setup_all_fdtd():
-    """
-    Most of this is copied from the `grid.visualize` method provided in
-    the fdtd module.
-    """
-    import fdtd
+if TYPE_CHECKING:
     import numpy as np
 
+
+def np_array_to_imagedata(img: "np.ndarray"):
+    """Convert `img` to the Javascript `ImageData` type,
+    which is useable in an HTML canvas context's putImageData. Source: ChatGPT"""
+    import numpy as np
+    assert img.ndim == 2
+    h, w = img.shape
+    img[img > 255] = 255
+    # Convert to RGBA, which Uint8ClampedArray expects 
+    rgba = np.zeros((h, w, 4), dtype=np.uint8)
+    rgba[:, :, 0] = img  # red?
+    rgba[:, :, 1] = img  # green?
+    rgba[:, :, 2] = img  # blue?
+    rgba[:, :, 3] = 255  # opacity / "alpha"
+    # Convert NumPy array -> JS Uint8ClampedArray
+    clamp = Uint8ClampedArray.new(rgba.tobytes())
+    return ImageData.new(clamp, w, h)
+
+
+def visualize_on_canvas(grid):
+    import numpy as np
     # relative
-    from fdtd.backend import backend as bd
-
-    def np_array_to_imagedata(img: "np.ndarray"):
-        """Convert `img` to the Javascript `ImageData` type,
-        which is useable in an HTML canvas context's putImageData. Source: ChatGPT"""
-        assert img.ndim == 2
-        h, w = img.shape
-        img[img > 255] = 255
-        # Convert to RGBA, which Uint8ClampedArray expects 
-        rgba = np.zeros((h, w, 4), dtype=np.uint8)
-        rgba[:, :, 0] = img  # red?
-        rgba[:, :, 1] = img  # green?
-        rgba[:, :, 2] = img  # blue?
-        rgba[:, :, 3] = 255  # opacity / "alpha"
-        # Convert NumPy array -> JS Uint8ClampedArray
-        clamp = Uint8ClampedArray.new(rgba.tobytes())
-        # Create and return JS ImageData
-        return ImageData.new(clamp, w, h)
-
-
-# 0 -> 85  would be 0 red to 255 red
-# 86 -> 170 would be 0 blue to max blue
-# 171 -> 255 would be 0 blue to max blue
-
-    def visualize_supersimple_v1(grid):
-        grid_energy_3d = bd.sum(grid.E ** 2 + grid.H ** 2, -1)  # type: ignore 
-        assert type(grid_energy_3d) == np.ndarray
-        grid_energy_xy = grid_energy_3d[:, :, 0]
-        geplus1 = grid_energy_xy
-        grid_energy_logscale = geplus1 * 50000
-        canvas = document.getElementById("simpledrawcanvas")
-        ctx = canvas.getContext("2d")
-        imageData = np_array_to_imagedata(grid_energy_logscale)
-        ctx.putImageData(imageData, 0, 0)
-
-
-    
-
-    fdtd.set_backend("numpy")
-    return visualize, visualize_supersimple_v1
-
+    from fdtd.backend import backend as bd  # type: ignore
+    grid_energy_3d = bd.sum(grid.E ** 2 + grid.H ** 2, -1)  # type: ignore 
+    assert type(grid_energy_3d) == np.ndarray
+    grid_energy_xy = grid_energy_3d[:, :, 0]
+    geplus1 = grid_energy_xy
+    grid_energy_logscale = geplus1 * 50000
+    canvas = document.getElementById("simpledrawcanvas")
+    ctx = canvas.getContext("2d")
+    imageData = np_array_to_imagedata(grid_energy_logscale)
+    ctx.putImageData(imageData, 0, 0)
 
 
 ######## WEB BROWSER STUFF
@@ -66,39 +50,31 @@ def print_to_div(*args):
 
 print = print_to_div
 
-persistvars = {}
 
-def exec_text(ev, el):
-    try:
-        if ev.key == "Enter":
-            v = el.value
-            print(">>>", v)
-            exec(v, globals(), persistvars)
-    except Exception as e:
-        print(e)
+if TYPE_CHECKING:
+    from typing_extensions import TypedDict
+    class MaterialProperty(TypedDict):
+        permittivity: float
+        conductivity: float
 
+materials: "dict[str, MaterialProperty]" = {
+    "metal": {"permittivity": 1e4, "conductivity": 1e5},  # source: ChatGPT
+    "dry sand": {"permittivity": 3.0, "conductivity": 1e-4},  # source: ChatGPT
+    "moist sand": {"permittivity": 10.0, "conductivity": 1e-2},  # source: ChatGPT
+    "fake1": {"permittivity": 4.5, "conductivity": 1e-3},  # source: made it up
+}
 
-
-async def loadgrid():
-    import fdtd
-    filename = "fdtdmap.txt"
-    response = await pyfetch(filename)
-    if not response.ok:
-        raise FileNotFoundError(f"Failed to load {filename} (status {response.status})")
-    text: str = await response.text()
+async def loadgrid(filename: str):
+    """roughly,
+    - read `filename`
+    - create a PointSource in spots with "p"
+    - create metal in spots with "m"
+    """
+    import fdtd  # type: ignore
+    from eml import read_text
+    text = await read_text(filename)
     lines = text.splitlines()
-    if TYPE_CHECKING:
-        from typing_extensions import TypedDict
-        class MaterialProperty(TypedDict):
-            permittivity: float
-            conductivity: float
-    materials: "dict[str, MaterialProperty]" = {
-            "metal": {"permittivity": 1e4, "conductivity": 1e5},  # source: ChatGPT
-            "dry sand": {"permittivity": 3.0, "conductivity": 1e-4},  # source: ChatGPT
-            "moist sand": {"permittivity": 10.0, "conductivity": 1e-2},  # source: ChatGPT
-            "fake1": {"permittivity": 4.5, "conductivity": 1e-3},  # source: made it up
-        }
-
+    fdtd.set_backend("numpy")
     grid = fdtd.Grid(
                 # height (y), width (x), depth (z)
         shape = (100, 200, 1),  # type: ignore
@@ -112,12 +88,7 @@ async def loadgrid():
                 grid[yidx, xidx, 0] = fdtd.AbsorbingObject(**materials["metal"])
             if char == "p":
                 grid[yidx, xidx, 0] = fdtd.PointSource(period = 60, amplitude=4, pulse=True, cycle=1) # type: ignore
-            
     return grid
-    ## idea:
-    ## read the file map.txt
-    ## create a PointSource in spots with "p"
-    ## create metal in spots with "m"
 
 
 async def main():
@@ -127,37 +98,10 @@ async def main():
         await pyodide_js.loadPackage("micropip")
         import micropip  # type: ignore
         await micropip.install("fdtd")
-        
-        visualize, visualize_supersimple_v1 = setup_all_fdtd()
-        grid = await loadgrid()
-        for unus in range(2000):
-            visualize_supersimple_v1(grid)
+        grid = await loadgrid("fdtdmap.txt")
+        for _unus in range(2000):
+            visualize_on_canvas(grid)
             grid.step() 
             await asyncio.sleep(0)
-
-
-        if False:
-            el = document.getElementById("execthis")
-            el.onkeyup = lambda event: exec_text(event, el)
-            exec("import fdtd", globals(), persistvars)
-            exec("visualize = setup_all_fdtd()", globals(), persistvars)
-            print("ready")
-            async def dostuff():
-                
-
-            
-
-            
-
-                try:
-                    visualize = globals()["persistvars"]["visualize"]
-                    # grid = globals()["persistvars"]["grid"]
-                    for unus in range(700):
-                        visualize(grid, z=0, animate=True, norm="log")
-                        grid.step() 
-                        await asyncio.sleep(0)  # apparently this allows the browser to refresh
-                except Exception as e:
-                    print("ex", e)
-            asyncio.create_task(dostuff())
     except Exception as e:
         print(e)
