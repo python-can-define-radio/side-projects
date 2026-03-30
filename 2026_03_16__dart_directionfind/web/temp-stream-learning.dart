1b213af9e86dd42c217f748ed658cbe0ebe49192
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2026-03-30 15:53:31 -0400

    Update temp-stream-learning.dart: brought more from `main.dart`. Basic movement working. Needs y pos stuff

--- 2026_03_16__dart_directionfind/web/temp-stream-learning.dart
+++ 2026_03_16__dart_directionfind/web/temp-stream-learning.dart
@@ -4,7 +4,11 @@ library;
 import 'dart:js_interop';
 import 'package:web/web.dart';
 import 'dart:async';
+import 'dart:math';
 
+
+const canvWidth = 600;
+const canvHeight = 400;
 
 /// Methods for creating HTML elems
 class HTML {
@@ -25,6 +29,20 @@ class HTML {
 }
 
 
+abstract class Drawable {
+  void draw(CanvasRenderingContext2D ctx, Pos center);
+}
+
+class TxRadio implements Drawable {
+  final pos = Pos(400, 370);
+  final txpower = Power(mW: 100);
+  
+  @override
+  void draw(CanvasRenderingContext2D ctx, Pos center) {
+    ctx.fillStyle = "#00f".toJS;
+    fillRectRel(pos.x, pos.y, 10, 10, ctx, center);
+  }
+}
 
 /// Repeatedly call requestAnimationFrame; pass the time delta as an argument to `frameUpdate`
 void runEachFrame(void Function(Duration) frameUpdate) {
@@ -56,6 +74,14 @@ extension Scanner<T> on Stream<T> {
 }
 
 
+class LatestStreamVal<T> {
+  T _val;
+  T get val => _val;
+  LatestStreamVal(Stream<T> stream, this._val) {
+    stream.listen((x) => _val = x);
+  }
+}
+
 class Pos {
   final double x;
   final double y;
@@ -65,24 +91,26 @@ class Pos {
 typedef KbStm = ElementStream<KeyboardEvent>;
 typedef DuStm = Stream<Duration>;
 
-class Player {
-  late final Stream<double> vxStm;
-  late final Stream<double> vyStm;
-  late final Stream<double> xStm;
+class Player implements Drawable {
+  late final Stream<double> _vxStm;
+  late final Stream<double> _vyStm;
+  late final Stream<double> _xStm;
+  late final LatestStreamVal<double> _xnow;
+  Pos get pos => Pos(_xnow.val, 370);
   static const speed = 0.2;
   Player(KbStm keydown, KbStm keyup, DuStm tdeltaStm) {
-    vxStm = _makeVxStream(0, keydown, keyup);
-    vyStm = _makeVyStream(0, keydown, keyup);
-    xStm = _makeXStream(0, vxStm, tdeltaStm);
+    const initX = 380.0;
+    _vxStm = _makeVxStream(0, keydown, keyup).asBroadcastStream();
+    _vyStm = _makeVyStream(0, keydown, keyup).asBroadcastStream();
+    _xStm = _makeXStream(initX, _vxStm, tdeltaStm).asBroadcastStream();
+    _xnow = LatestStreamVal(_xStm, initX);
   }
   
   static Stream<double> _makeXStream(double initX, Stream<double> vxStm, DuStm tdeltaStm) async* {
-    yield initX;
     var curX = initX;
-    double curVx = 0;
-    vxStm.listen((vx) => curVx = vx);
+    var curVx = LatestStreamVal(vxStm, 0);
     await for(final tdelta in tdeltaStm) {
-      curX += curVx * tdelta.inMilliseconds;
+      curX += curVx.val * tdelta.inMilliseconds;
       yield curX;
     }
   }
@@ -124,45 +152,161 @@ class Player {
   HTMLDivElement disp() {
     final vyEl = HTML.span()..style.color = "yellow";
     final xEl = HTML.span()..style.color = "lightgreen";
-    vyStm.listen((vy) => vyEl.innerText = "vy: $vy ");
-    xStm.listen((x) => xEl.innerText = "x: $x");
+    _vyStm.listen((vy) => vyEl.innerText = "vy: $vy ");
+    _xStm.listen((x) => xEl.innerText = "x: $x");
     return HTML.div()
       ..appendChild(vyEl)
       ..appendChild(xEl);
   }
+  
+  @override
+  void draw(CanvasRenderingContext2D ctx, Pos _) {
+    ctx.fillStyle = "#f00".toJS;
+    ctx.fillRect(300, 200, 30, 40);
+  }
 }
 
-void attachElems(HTMLElement root, Player p1, LOBCol lobc){
-  root
-    ..appendChild(p1.disp())
-    ..appendChild(lobc.disp());
+
+void fillRectRel(
+  num x,
+  num y,
+  num w,
+  num h,
+  CanvasRenderingContext2D ctx,
+  Pos relpos,
+) {
+  ctx.fillRect(
+    x - relpos.x + canvWidth / 2,
+    y - relpos.y + canvHeight / 2,
+    w,
+    h,
+  );
 }
+
+void moveToRel(num x, num y, CanvasRenderingContext2D ctx, Pos relpos) {
+  ctx.moveTo(x - relpos.x + canvWidth / 2, y - relpos.y + canvHeight / 2);
+}
+
+void lineToRel(num x, num y, CanvasRenderingContext2D ctx, Pos relpos) {
+  ctx.lineTo(x - relpos.x + canvWidth / 2, y - relpos.y + canvHeight / 2);
+}
+
+void fillCircle(num x, num y, num radius, CanvasRenderingContext2D ctx) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * pi);
+  ctx.fill();
+}
+
+void fillCircleRel(
+  num x,
+  num y,
+  num radius,
+  CanvasRenderingContext2D ctx,
+  Pos relpos,
+) {
+  fillCircle(
+    x - relpos.x + canvWidth / 2,
+    y - relpos.y + canvHeight / 2,
+    radius,
+    ctx,
+  );
+}
+
+
+class CanvM {
+  final String _bgcolor;
+  final _canv = HTML.canvas();
+  late final CanvasRenderingContext2D _ctx;
+
+  CanvM(this._bgcolor, int w, int h, Stream<Duration> frameStm, Player p1, List<Drawable> drawItems) {
+    _canv
+      ..width = w
+      ..height = h;
+    _ctx = _canv.getContext('2d') as CanvasRenderingContext2D;
+    frameStm.listen((Duration tdelta) {
+      _drawBackground();
+      for (final item in drawItems) {
+        item.draw(_ctx, p1.pos);
+      }
+    });
+  }
+
+  void _drawBackground() {
+    _ctx.fillStyle = _bgcolor.toJS;
+    _ctx.fillRect(0, 0, _canv.width, _canv.height);
+  }
+  
+  HTMLCanvasElement disp() => _canv;
+}
+
+
+
 
 Stream<Duration> makeFrameStm() {
   final timeDiffSC = StreamController<Duration>();
   runEachFrame((Duration tdelta) => timeDiffSC.add(tdelta));
-  return timeDiffSC.stream;
+  return timeDiffSC.stream.asBroadcastStream();
 }
+
+
+typedef LOB = ({Pos source, Azimuth azimuth, Power rxpow});
+
 
 class LOBCol {
-  late final List<double> _lobs = [];
+  final List<LOB> _lobs = [];
+  final _gatheringLobsCb = HTML.checkbox()..defaultChecked = true;
 
+  LOBCol(KbStm keydown) {
+    keydown
+      .where((ev) => ev.key.toLowerCase() == "g")
+      .listen((_) => 
+        _gatheringLobsCb.checked = !_gatheringLobsCb.checked);
+    keydown
+      .where((ev) => ev.key.toLowerCase() == "c")
+      .listen((_) => _lobs.clear());
+  }
   HTMLDivElement disp() {
-    final clearBtn = HTML.button()
-      ..innerText = "Clear Lobs"
-      ..onClick.listen((_) => _lobs.clear());
     return HTML.div()
-      ..appendChild(clearBtn);
+      ..appendChild(HTML.div()
+        ..appendChild(HTML.span()..innerText = "Gathering Lobs [ g ]:")
+        ..appendChild(_gatheringLobsCb))
+      ..appendChild(HTML.button()
+        ..innerText = "Clear LOBs [ c ]"
+        ..onClick.listen((_) => _lobs.clear()));
   }
 }
+
+class Azimuth {}
+
+double logbase10(double x) => log(x) / log(10);
+
+class Power {
+  final double mW;
+  double get dBm => 10 * logbase10(mW);
+  Power({required this.mW});
+  Power operator *(double other) {
+    return Power(mW: mW * other);
+  }
+}
+
+
+void attachElems(HTMLElement root, Player p1, LOBCol lobc, CanvM cmLife){
+  root
+    ..appendChild(p1.disp())
+    ..appendChild(lobc.disp())
+    ..appendChild(cmLife.disp());
+}
+
 
 void main() {
   final keydown = document.body!.onKeyDown;
   final keyup = document.body!.onKeyUp;
   final frameStm = makeFrameStm();
   final p1 = Player(keydown, keyup, frameStm);
-  final lobc = LOBCol();
-  attachElems(document.body!, p1, lobc);
+  final lobc = LOBCol(keydown);
+  final t1 = TxRadio();
+  final cmLife = CanvM("#cfc", canvWidth, canvHeight, frameStm, p1, [p1, t1]);
+  attachElems(document.body!, p1, lobc, cmLife);
 }
 
 
